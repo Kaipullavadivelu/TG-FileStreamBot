94320c648d5b66df33ab28043604e353ceca5cbb
Kaipullavadivelu <86786455+Kaipullavadivelu@users.noreply.github.com>
2021-12-24 08:26:13 +0530

    Update __init__.py

--- WebStreamer/__init__.py
+++ WebStreamer/__init__.py
@@ -1,24 +1,10 @@
 # This file is a part of TG-FileStreamBot
 # Coding : Jyothis Jayanth [@EverythingSuckz]
 
-import logging
-import os
+
 import time
-import random
-import string
-import subprocess
-import requests
 from .vars import Var
 from WebStreamer.bot import StreamBot
-
-import telegram.ext as tg
-from dotenv import load_dotenv
-
-load_dotenv('config.env')
-
-def getConfig(name: str):
-    return os.environ[name]
- 
 
 print('\n')
 print('------------------- Initalizing Telegram Bot -------------------')
@@ -27,14 +13,4 @@ StreamBot.start()
 bot_info = StreamBot.get_me()
 __version__ = 1.06
 StartTime = time.time()
-
- try:
-    SHORTENER = getConfig('SHORTENER')
-    SHORTENER_API = getConfig('SHORTENER_API')
-    if len(SHORTENER) == 0 or len(SHORTENER_API) == 0:
-        raise KeyError
-except KeyError:
-    SHORTENER = None
-    SHORTENER_API = None
-
 
