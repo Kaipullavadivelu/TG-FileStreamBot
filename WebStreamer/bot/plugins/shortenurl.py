3a871679bfe12de2701fbeba75a022341ab71a1a
Kaipullavadivelu <86786455+Kaipullavadivelu@users.noreply.github.com>
2021-12-24 08:38:33 +0530

    Update shortenurl.py

--- WebStreamer/bot/plugins/shortenurl.py
+++ WebStreamer/bot/plugins/shortenurl.py
@@ -6,7 +6,7 @@ import base64
 import pyshorteners
 from urllib.parse import quote
 from urllib3 import disable_warnings
-from .vars import SHORTENER, SHORTENER_API
+from WebStreamer import SHORTENER, SHORTENER_API
 
 
 def short_url(longurl):
