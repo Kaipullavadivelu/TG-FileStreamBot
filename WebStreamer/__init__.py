5012fe91288f3b99bc763890d8fcbe0f6e5891c7
Kaipullavadivelu <86786455+Kaipullavadivelu@users.noreply.github.com>
2021-12-24 08:25:31 +0530

    Update __init__.py

--- WebStreamer/__init__.py
+++ WebStreamer/__init__.py
@@ -18,15 +18,7 @@ load_dotenv('config.env')
 
 def getConfig(name: str):
     return os.environ[name]
-  
-  try:
-    SHORTENER = getConfig('SHORTENER')
-    SHORTENER_API = getConfig('SHORTENER_API')
-    if len(SHORTENER) == 0 or len(SHORTENER_API) == 0:
-        raise KeyError
-except KeyError:
-    SHORTENER = None
-    SHORTENER_API = None
+ 
 
 print('\n')
 print('------------------- Initalizing Telegram Bot -------------------')
@@ -35,5 +27,14 @@ StreamBot.start()
 bot_info = StreamBot.get_me()
 __version__ = 1.06
 StartTime = time.time()
+
+ try:
+    SHORTENER = getConfig('SHORTENER')
+    SHORTENER_API = getConfig('SHORTENER_API')
+    if len(SHORTENER) == 0 or len(SHORTENER_API) == 0:
+        raise KeyError
+except KeyError:
+    SHORTENER = None
+    SHORTENER_API = None
 
 
